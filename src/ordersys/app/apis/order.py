abaed5e3fead1511c7c79b60f3feddd9888c4851
Tan Yuanhong <leotan.yh@gmail.com>
2022-05-27 02:08:06 +0800

    src: update Order API with extra actions

--- src/ordersys/app/apis/order.py
+++ src/ordersys/app/apis/order.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework import filters, decorators, response
 from rest_framework import viewsets, permissions
 
 from app.models import Order
@@ -13,3 +13,22 @@ class OrderViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ['status', 'table']
     ordering_fields = ['date']
+
+    @decorators.action(detail=True, methods=['get'])
+    def deliver_order(self, request, pk=None):
+        order = self.get_object()
+        order.status = Order.STATUS_DELIVERED
+        order.save()
+        return response.Response({
+            'status': 'ok',
+            'order': OrderSerializer(order).data
+        })
+
+    @decorators.action(detail=False, methods=['get'])
+    def deliver_all_orders(self, request):
+        for order in Order.objects.all():
+            order.status = Order.STATUS_DELIVERED
+            order.save()
+        return response.Response({
+            'status': 'ok'
+        })
